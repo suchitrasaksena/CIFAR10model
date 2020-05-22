05f7c1e5038fe18be6fa5ab93847a08c654f84ea
suchitrasaksena <suchitrasaksena@gmail.com>
2020-05-22 16:32:48 +0530

    model creation

---
+++ cifar10model.py
@@ -0,0 +1,38 @@
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Conv2D, MaxPool2D, Flatten
+from keras.utils import np_utils
+
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+X_train = X_train.reshape(X_train.shape[0], 32, 32, 3)
+X_test = X_test.reshape(X_test.shape[0], 32, 32, 3)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+
+X_train /= 255
+X_test /= 255
+
+n_classes = 10
+print("Shape before one-hot encoding: ", y_train.shape)
+Y_train = np_utils.to_categorical(y_train, n_classes)
+Y_test = np_utils.to_categorical(y_test, n_classes)
+print("Shape after one-hot encoding: ", Y_train.shape)
+
+model.add(Conv2D(75, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu'))
+model.add(MaxPool2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+
+model.add(Dense(500, activation='relu'))
+model.add(Dropout(0.4))
+model.add(Dense(250, activation='relu'))
+model.add(Dropout(0.3))
+
+model.add(Dense(10, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
+
+model.fit(X_train, Y_train, batch_size=128, epochs=10, validation_data=(X_test, Y_test))
+

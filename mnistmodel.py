8f2dd09d7bcbb9bb51aaf229a81a027b8cd38765
suchitrasaksena <suchitrasaksena@gmail.com>
2020-05-25 19:30:55 +0530

    model creation

---
+++ mnistmodel.py
@@ -0,0 +1,31 @@
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Conv2D, MaxPool2D
+from keras.utils import np_utils
+
+X_train = X_train.reshape(60000, 784)
+X_test = X_test.reshape(10000, 784)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+
+X_train /= 255
+X_test /= 255
+
+n_classes = 10
+Y_train = np_utils.to_categorical(y_train, n_classes)
+Y_test = np_utils.to_categorical(y_test, n_classes)
+
+model = Sequential()
+
+model.add(Dense(100, input_shape=(784,), activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
+
+history = model.fit(X_train, Y_train, batch_size=128, epochs=10, validation_data=(X_test, Y_test))
+
+model.save("mnistmodel")
+
+f = open ("cifar_accuracy.txt",'w')
+f.write('%d' % int(history.history['accuracy'][0] * 100))
+f.close()
